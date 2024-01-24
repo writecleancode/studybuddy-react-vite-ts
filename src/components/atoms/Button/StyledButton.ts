9cfbdce033e995934638b6bc9c7beb12d89f3c67
Rafal <rafek131@wp.pl>
2024-01-24 14:40:31 +0100

    'News Feed Section' is now displaying fetched articles

--- src/components/atoms/Button/StyledButton.ts
+++ src/components/atoms/Button/StyledButton.ts
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
-export const Button = styled.button`
+export const Button = styled.button<{ $isBig: boolean }>`
 	margin: 8px 0;
-	padding: 8px 16px;
+	padding: ${({ $isBig }) => ($isBig ? '8px 32px' : '8px 16px')};
 	border: none;
 	border-radius: 100px;
 	background-color: ${({ theme }) => theme.colors.lightPurple};
 	color: ${({ theme }) => theme.colors.darkGrey};
-	font-size: ${({ theme }) => theme.fontSize.s};
+	font-size: ${({ $isBig, theme }) => ($isBig ? theme.fontSize.m : theme.fontSize.s)};
 	font-weight: bold;
 `;
--- src/components/templates/NewsSection/NewsSection.tsx
+++ src/components/templates/NewsSection/NewsSection.tsx
@@ -1,54 +1,78 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Button } from 'src/components/atoms/Button/StyledButton';
 import { ArticleWrapper, ContentWrapper, NewsSectionHeader, TitleWrapper, Wrapper } from './NewsSection.styles';
 
+type articleType = {
+	id: string;
+	title: string;
+	category: string;
+	content: string;
+	image?: {
+		url: string;
+		alt: string;
+	};
+};
+
+const DATO_TOKEN = 'da4e120f3de32f16d2570730ea60a2';
+
 export const NewsSection = () => {
+	const [articles, setArticles] = useState<never[] | articleType[]>([]);
+	const [error, setError] = useState('');
+
+	useEffect(() => {
+		axios
+			.post(
+				'https://graphql.datocms.com/',
+				{
+					query: `
+						{
+							allArticles {
+								id
+								title
+								category
+								content
+								image {
+									url
+									alt
+								}
+							}
+						}
+					`,
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${DATO_TOKEN}`,
+					},
+				}
+			)
+			.then(({ data: { data } }) => setArticles(data.allArticles))
+			.catch(err => {
+				setError('Sorry, we could not load the articles...');
+				console.log(err);
+			});
+	}, []);
+
 	return (
 		<Wrapper>
 			<NewsSectionHeader>University News Feed</NewsSectionHeader>
-
-			<ArticleWrapper>
-				<TitleWrapper>
-					<h3>Title</h3>
-					<p>Category</p>
-				</TitleWrapper>
-				<ContentWrapper>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quae illum mollitia perferendis facilis
-						deserunt? Sapiente, hic, delectus doloribus molestiae aspernatur eaque, iste natus odit tenetur cumque
-						placeat voluptatum ut?
-					</p>
-				</ContentWrapper>
-				<Button>Read more</Button>
-			</ArticleWrapper>
-			<ArticleWrapper>
-				<TitleWrapper>
-					<h3>Title</h3>
-					<p>Category</p>
-				</TitleWrapper>
-				<ContentWrapper>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quae illum mollitia perferendis facilis
-						deserunt? Sapiente, hic, delectus doloribus molestiae aspernatur eaque, iste natus odit tenetur cumque
-						placeat voluptatum ut?
-					</p>
-				</ContentWrapper>
-				<Button>Read more</Button>
-			</ArticleWrapper>
-			<ArticleWrapper>
-				<TitleWrapper>
-					<h3>Title</h3>
-					<p>Category</p>
-				</TitleWrapper>
-				<ContentWrapper>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quae illum mollitia perferendis facilis
-						deserunt? Sapiente, hic, delectus doloribus molestiae aspernatur eaque, iste natus odit tenetur cumque
-						placeat voluptatum ut?
-					</p>
-				</ContentWrapper>
-				<Button>Read more</Button>
-			</ArticleWrapper>
-            
+			{articles.length ? (
+				articles.map(({ id, title, category, content, image = null }) => (
+					<ArticleWrapper key={id}>
+						<TitleWrapper>
+							<h3>{title}</h3>
+							<p>{category}</p>
+						</TitleWrapper>
+						<ContentWrapper>
+							<p>{content}</p>
+							{image ? <img src={image.url} alt={image.alt} /> : null}
+						</ContentWrapper>
+						<Button $isBig>Read more</Button>
+					</ArticleWrapper>
+				))
+			) : (
+				<NewsSectionHeader>{error ? error : 'Loading...'}</NewsSectionHeader>
+			)}
 		</Wrapper>
 	);
 };
