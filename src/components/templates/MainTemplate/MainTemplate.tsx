68c65dcf265b346cb8df97715289dd921620bcd6
Rafal <rafek131@wp.pl>
2024-01-27 23:12:06 +0100

    wrote test of 'SearchBar' component

--- src/components/templates/MainTemplate/MainTemplate.tsx
+++ src/components/templates/MainTemplate/MainTemplate.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 import { Navigation } from 'src/components/organisms/Navigation/Navigation';
 import { SearchBar } from 'src/components/organisms/SearchBar/SearchBar';
-import { NewsSection } from '../NewsSection/NewsSection';
+import { NewsSection } from 'src/components/templates/NewsSection/NewsSection';
 import { Wrapper } from './MainTemplate.styles';
 
 type MainTemplateProps = {
