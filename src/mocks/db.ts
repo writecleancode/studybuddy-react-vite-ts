4d385ae349d7b597dda2d247ebcc6f3ddb0af16d
Rafal <rafek131@wp.pl>
2024-01-31 14:43:56 +0100

    added user to mocked database for future authentication

--- src/mocks/db.ts
+++ src/mocks/db.ts
@@ -37,4 +37,11 @@ export const db = factory({
 	group: {
 		id: primaryKey(String),
 	},
+
+	teacher: {
+		id: primaryKey(() => '1'),
+		name: () => 'Jacek Sobczak',
+		login: () => 'teacher@studybuddy.com',
+		password: () => '1234',
+	},
 });
